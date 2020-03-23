02296d6e8afc8ec24f6aecd14d31405b39d0e806
ZeroVM-dot <xandevmware@gmail.com>
2020-03-23 00:43:27 -0300

    New Project

---
+++ loops0.py
@@ -0,0 +1,6 @@
+for i in range(5):
+    print(i)
+print("=====Other Loop type=====")
+names = ["Alice", "Bob", "Charlie"]
+for name in names:
+    print(name)
---
+++ variable.py
@@ -0,0 +1,11 @@
+i = 28
+print(f"I is {28}")
+
+f = 2.8
+print(f"F is {f}")
+
+b = False
+print(f"B is {b}")
+
+n = None
+print(f"N is {n}")
